6776cf18b898251310b52dcfd8f88d83bfff232b
Usman22209 <166890135+Usman22209@users.noreply.github.com>
2025-10-04 20:11:57 +0500

    Packages and splash added

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-worklets/plugin', 
+    ['module:react-native-dotenv'],
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.ts',
+          '.tsx',
+          '.json',
+          '.svg',
+        ],
+        alias: {
+          '@assets': './src/assets',
+          '@theme': './src/shared/theme',
+          '@components': './src/shared/components',
+        },
+      },
+    ],
+  ],
 };
