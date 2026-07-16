a6061ab1b5bde8f3e10c02072f9a551595ab8296
Usman Shafiq <usman.shafiq.dev@gmail.com>
2026-07-16 16:02:22 +0500

    ios icons added

--- react-native.config.js
+++ react-native.config.js
@@ -3,6 +3,9 @@ module.exports = {
     ios: {},
     android: {},
   },
-  assets: ["./src/assets/fonts"],
+  assets: [
+    "./src/assets/fonts",
+    "./node_modules/react-native-vector-icons/Fonts"
+  ],
 };
 //npx react-native-asset
